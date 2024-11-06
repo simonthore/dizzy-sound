d5916eb8eadc1efca6e2427ff368e82bc5798f85
Simon <simon.thore64000@gmail.com>
2024-11-06 16:38:12 +0100

    save navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
+import { ArtistsComponent } from '../components/artists/artists.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { HomeComponent } from '../components/home/home.component';
+import { EventsComponent } from '../components/events/events.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ArtistsComponent,
+    ContactComponent,
+    HomeComponent,
+    EventsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,11 @@ import { Routes } from '@angular/router';
 import { ArtistsComponent } from '../components/artists/artists.component';
 import { ContactComponent } from '../components/contact/contact.component';
 import { HomeComponent } from '../components/home/home.component';
-import { HeaderComponent } from '../components/header/header.component';
 import { EventsComponent } from '../components/events/events.component';
 
-
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'header', component: HeaderComponent },
-    { path: 'events', component: EventsComponent },
-    { path: 'contact', component: ContactComponent },
-    {path: 'artists', component: ArtistsComponent}
-  ];
+  { path: 'home', component: HomeComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'artists', component: ArtistsComponent },
+];
