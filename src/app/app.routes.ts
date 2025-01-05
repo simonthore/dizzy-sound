c2b821ecba52add5f03d12d7bbae011c298b356e
Simon Thore <simon.thore@hotmail.fr>
2025-01-05 11:51:46 +0100

    merge ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
-
+import { HeaderComponent } from '../components/header/header.component';
 import { ArtistsComponent } from '../components/artists/artists.component';
 import { ContactComponent } from '../components/contact/contact.component';
 import { HomeComponent } from '../components/home/home.component';
 import { EventsComponent } from '../components/events/events.component';
+import { SplashScreenComponent } from '../components/splash-screen/splash-screen.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'events', component: EventsComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'artists', component: ArtistsComponent },
-];
+
+    { path: '', redirectTo: '/splash-screen', pathMatch: 'full' },  // Redirection initiale vers l'écran de présentation
+    { path: 'splash-screen', component: SplashScreenComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'header', component: HeaderComponent },
+    { path: 'events', component: EventsComponent },
+    { path: 'contact', component: ContactComponent },
+    {path: 'artists', component: ArtistsComponent}
+  ];
